58e1645fc7e617609a760037b8e849021eb278db
lucasmartinsribeiro <lucasmartins0498@gmail.com>
2022-10-31 20:45:26 -0300

    update

--- lab-jenkins/provision.sh
+++ lab-jenkins/provision.sh
@@ -11,6 +11,7 @@ service jenkins start
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli  containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
